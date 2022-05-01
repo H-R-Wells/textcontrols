d517a24b778d57b701d66d59ea7075bfb273e1d6
H. R. Wells <100534001+H-R-Wells@users.noreply.github.com>
2022-05-02 02:38:03 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import About from "./components/about";
 import Navbar from "./components/navbar";
-import TextForm from "./components/textForm";
+// import TextForm from "./components/textForm";
 
 function App() {
   return (
     <>
       <Navbar title="React bitch" aboutText="About" />
-      <TextForm  />
+      {/* <TextForm/> */}
+      <About/>
     </>
   );
 }
